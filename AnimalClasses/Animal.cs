ed790b4f3c4eb7b325de3b9ba3e44f6474e43c62
Axmart <notaxmart@gmail.com>
2020-09-05 19:18:52 +0200

    Add project files.

---
+++ AnimalClasses/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClasses
+{
+    class Animal
+    {
+
+        public bool IsAlive { get; set; }
+        public int Weight { get; set; }
+
+        public Animal()
+        {
+            Console.WriteLine("Animal Constructor");
+        }
+
+        public Animal(bool isAlive, int weight)
+        {
+            Console.WriteLine("Animal Constructor");
+
+            IsAlive = isAlive;
+            Weight = weight;
+
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Animal says hello");
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine("Animal is talking");
+        }
+
+        public void Sing()
+        {
+            Console.WriteLine("Animal is singing");
+        }
+
+        public string ToString()
+        {
+
+            return $"Status : {IsAlive}  Weight : {Weight}";
+
+        }
+
+
+    }
+}
